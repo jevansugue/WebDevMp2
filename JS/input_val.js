978a4e0c23937d834cec68d1fe094e677e36ab0e
Kim Blanquera <kim.blanquera@gmail.com>
2013-07-14 19:24:13 +0800

    more js stuff

---
+++ JS/input_val.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+//VALIDATION FUNCTIONS
+
+//REQUIRED FIELDS VALDATION (C) W3CSCHOOLS
+
+function validateForm()
+{
+var x=document.forms["ordnum"]["orderno"].value;
+if (x==null || x=="")
+  {
+  alert("Order Number must be filled.");
+  return false;
+  }
+}
